af42d69328044a83fe9c20e5625468a9767207a8
Dominic-Bartlomowicz <dbartlomowicz@gmail.com>
2018-03-02 20:33:09 -0800

    second commit

--- summation.js
+++ summation.js
@@ -1,22 +1,25 @@
-// A function which sums any number from i to n, based on a function entered by the user
-
+// A function which sums any number from i to n, based on a function of x entered by the user
 
 function summation(i,n)
 
 {
 
+var sum = 0; // Initial value of sum
 
-var sum = 0; // Initial value
-
-
-for (var num = i; num <= n; num++)
+for (var x = i; x <= n; x++)
 
 	{
 
-		// Let f(x) = 2x, where x = num
+		// Let f(x) = 2x (modify for other functions)
 
-		sum += 2*(num);
+		sum += 2*(x);
 
 	}
 
 return sum;
+
+}
+
+// Test: i = 1 to n = 5, expected output = (2*1)+(2*2)+(2*3)+(2*4)+(2*5) = 30
+
+console.log(summation(1,5));
