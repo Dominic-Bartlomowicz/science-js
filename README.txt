465742ed23428ec6d8a88d37767414895f2f22db
Dominic-Bartlomowicz <dbartlomowicz@gmail.com>
2018-03-08 12:16:50 -0800

    second commit

---
+++ README.txt
@@ -0,0 +1,13 @@
+This is a repository of potentially useful javascript programs that are related to the fields of math and science.
+
+
+The files are written as either functions or programs, and this is specified in the comments at the headings of the individual scripts.
+
+
+In order to run the programs, you will need to install the files package.json and package-lock.json in your respective directory, followed by the node_modules folder. These files and folder are necessary for the user to parse an input into the program that can be interpreted by javascript. 
+
+
+
+ 
+
+ 
--- prime.js
+++ prime.js
@@ -1,9 +1,15 @@
 
-// A simple function that determines whether a number n is a prime number.
+// A simple program that determines whether a number n is a prime number.
 
-function prime(n)
 
 {
+
+console.log("");
+
+var prompt = require("prompt-sync")();
+var n = prompt("Please enter a positive integer: ");
+console.log("");
+
 
 for(var i = 2; i < n; i++)
 
@@ -11,20 +17,15 @@ for(var i = 2; i < n; i++)
 		if(n%i == 0)
 
 			{
-				return(console.log(n + " is not a prime number"));
+						return(console.log(n + " is not a prime number\n"));
 			}
 				
 	}
 
+	
 console.log((n + " is a prime number"));
+
 
 }
 
 
-// Testing prime numbers (3,5,7) and non-prime (8,10)
-
-prime(3);
-prime(5);
-prime(7);
-prime(8);
-prime(10);
