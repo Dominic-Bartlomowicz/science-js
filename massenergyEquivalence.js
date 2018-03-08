d6709086d7995c4670689a54f19553812acd618e
Dominic-Bartlomowicz <dbartlomowicz@gmail.com>
2018-03-08 11:45:56 -0800

    second commit

--- massenergyEquivalence.js
+++ massenergyEquivalence.js
@@ -1,15 +1,36 @@
-// A simple Mass-Energy equivalence function, first derived by theoretical physicist Albert Einstein in the year 1905.  
+// A simple Mass-Energy equivalence calculator, first derived by theoretical physicist Albert Einstein in the year 1905.  
 
-function energy(mass)
 
 {
 
-return mass * (299792458 * 299792458);
+while(true)
+
+	{
+
+		console.log("");
+
+		var prompt = require("prompt-sync")();
+		var mass = prompt("Please enter a mass value in kilograms: ");
+
+		while (isNaN(mass) || mass <= 0)
+
+			{
+				console.log("");
+				var mass = prompt("Invalid value. Please enter a mass value in kilograms: ");
+				console.log("");
+			}
+		
+
+		console.log("");
+
+		var energy = mass * (299792458 * 299792458);
+
+		console.log("The energy energy calculated is " + energy + " joules"); 
+
+		console.log("");
+
+	}
 
 }
 
-
-// Test for 0.0000002 kg Mass
-
-console.log("The energy calculated is " + energy(0.0000002) + " joules");
 
